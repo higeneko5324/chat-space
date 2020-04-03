2221fb6d8f6c9d0c913f3a4fcbf742edece4e5e3
higeneko5324 <tetsu5324@icloud.com>
2020-04-03 21:27:26 +0900

    update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -36,10 +36,13 @@ $(function(){
     };
     return html;
    }
+   
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
+
     
     $.ajax({
       url: url,
