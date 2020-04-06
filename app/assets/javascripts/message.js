f8f4bb6df644ea572d3724e0c86bf70d2bdb972c
higeneko5324 <tetsu5324@icloud.com>
2020-04-06 14:08:07 +0900

    last-update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -64,7 +64,6 @@ $(function(){
   var reloadMessages = function() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     var last_message_id = $('.chat-main__message__list:last').data("message-id");
-    console.log(last_message_id)
     $.ajax({
       //ルーティングで設定した通りのURLを指定
       url: "api/messages",
